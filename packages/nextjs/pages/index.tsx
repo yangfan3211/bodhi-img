6fa1632ad5fc0dd9467783f330fe2d3e9a5647b0
Yangfan <chenyangfan3211@gmail.com>
2024-05-14 15:43:24 +0800

    🎨 use swr

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { NextPage } from "next";
+import swr, { useSWRConfig } from "swr";
 import { ImgShow } from "~~/components/ImgShow";
 
 const fetcher = (...args) => fetch(...args).then(res => res.json());
@@ -11,78 +12,141 @@ const ETHSpace: NextPage = () => {
   const [loading, setLoading] = useState(false);
   const [maxPage, setMaxPage] = useState(1);
   const onNextPage = () => {
-    // if (pageIndex < maxPage) {
-    setPageIndex(pageIndex + 1);
-    // }
+    if (pageIndex < maxPage) {
+      setPageIndex(pageIndex + 1);
+      mutate("https://bodhi-data.deno.dev/imgs_latest_id");
+    }
   };
 
   const onPrevPage = () => {
     if (pageIndex > 1) {
       setPageIndex(pageIndex - 1);
+      mutate("https://bodhi-data.deno.dev/imgs_latest_id");
     }
   };
   // Fetch images from your backend
-  const fetchImages = async (page: number) => {
-    setLoading(true);
-    try {
-      const categories = await fetch("https://bodhi-data.deno.dev/constant?key=bodhi_img_categories", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const categoriesData = await categories.json();
-      console.log(categoriesData);
-      const reg = /\\/g;
+  // const fetchImages = async (page: number) => {
+  //   setLoading(true);
+  //   try {
+  //     const categories = await fetch("https://bodhi-data.deno.dev/constant?key=bodhi_img_categories", {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     const categoriesData = await categories.json();
+  //     console.log(categoriesData);
+  //     const reg = /\\/g;
 
-      const replaceAfter = categoriesData.value.replace(reg, "");
+  //     const replaceAfter = categoriesData.value.replace(reg, "");
+  //     const categoriesString = replaceAfter.substring(2, replaceAfter.length - 2);
+  //     const categoriesArray = categoriesString.split('","');
+  //     setCategories(categoriesArray);
+  //     // const cursorResponse = await fetch("https://bodhi-data.deno.dev/imgs_latest_id", {
+  //     //   method: "GET",
+  //     //   headers: {
+  //     //     "Content-Type": "application/json",
+  //     //   },
+  //     // });
+  //     // const lastedIdData = await cursorResponse.json();
+  //     // const cursor = lastedIdData.latestId - (page - 1) * 10;
+  //     // setMaxPage(Math.ceil(lastedIdData.latestId / 10));
+  //     // const categoryParams = category === "All" ? "" : `&category=${category}`;
+  //     // const response = await fetch(`https://bodhi-data.deno.dev/imgs?cursor=${cursor}&limit=10${categoryParams}`, {
+  //     //   method: "GET",
+  //     //   headers: {
+  //     //     "Content-Type": "application/json",
+  //     //   },
+  //     // });
+
+  //     const categoryParams = category === "All" ? "" : `&category=${category}`;
+  //     const response = await fetch(`https://bodhi-data.deno.dev/imgs_page?page=${page}&limit=10${categoryParams}`, {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     const data = await response.json();
+  //     console.log(data);
+  //     // Assuming the data format matches what the ImgShow component expects
+  //     const formattedImages = data.images.map((img: { link: any; id_on_chain: string; category: string }) => ({
+  //       image: img.link,
+  //       link: "https://bodhi.wtf/" + img.id_on_chain,
+  //       category: img.category || "",
+  //       id: img.id_on_chain || 0,
+  //     }));
+  //     setImages(formattedImages);
+  //   } catch (error) {
+  //     console.error("Failed to fetch images:", error);
+  //   }
+  //   setLoading(false);
+  // };
+
+  // useEffect(() => {
+  //   fetchImages(pageIndex);
+  // }, [pageIndex, category]);
+  const { mutate } = useSWRConfig();
+  const [shouldFetchImg, setShouldFetchImg] = useState(false);
+  const [cursor, setCursor] = useState(0);
+  const [shouldCursor, setShouldCursor] = useState(false);
+  const [categoryParams, setCategoryParams] = useState("");
+  const { data: categoriesRes, isLoading: categoriesLoading } = swr(
+    "https://bodhi-data.deno.dev/constant?key=bodhi_img_categories",
+    fetcher,
+  );
+
+  const {
+    data: cursorRes,
+    isLoading: cursorLoading,
+    isValidating: cursorValidating,
+  } = swr(shouldCursor ? "https://bodhi-data.deno.dev/imgs_latest_id" : null, fetcher);
+  const { data: imgRes, isLoading: responseLoading } = swr(
+    shouldFetchImg ? `https://bodhi-data.deno.dev/imgs?cursor=${cursor}&limit=10${categoryParams}` : null,
+    fetcher,
+  );
+
+  useEffect(() => {
+    setLoading(categoriesLoading || cursorLoading || responseLoading);
+  }, [categoriesLoading, cursorLoading, responseLoading]);
+
+  useEffect(() => {
+    if (categoriesRes) {
+      const reg = /\\/g;
+      const replaceAfter = categoriesRes.value.replace(reg, "");
       const categoriesString = replaceAfter.substring(2, replaceAfter.length - 2);
       const categoriesArray = categoriesString.split('","');
       setCategories(categoriesArray);
-      // const cursorResponse = await fetch("https://bodhi-data.deno.dev/imgs_latest_id", {
-      //   method: "GET",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      // });
-      // const lastedIdData = await cursorResponse.json();
-      // const cursor = lastedIdData.latestId - (page - 1) * 10;
-      // setMaxPage(Math.ceil(lastedIdData.latestId / 10));
-      // const categoryParams = category === "All" ? "" : `&category=${category}`;
-      // const response = await fetch(`https://bodhi-data.deno.dev/imgs?cursor=${cursor}&limit=10${categoryParams}`, {
-      //   method: "GET",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      // });
+      setShouldCursor(true);
+      // mutate("https://bodhi-data.deno.dev/imgs_latest_id");
+    }
+  }, [categoriesRes]);
 
-      const categoryParams = category === "All" ? "" : `&category=${category}`;
-      const response = await fetch(`https://bodhi-data.deno.dev/imgs_page?page=${page}&limit=10${categoryParams}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await response.json();
-      console.log(data);
-      // Assuming the data format matches what the ImgShow component expects
-      const formattedImages = data.images.map((img: { link: any; id_on_chain: string; category: string }) => ({
+  useEffect(() => {
+    console.log(cursorRes);
+    if (cursorRes && cursorValidating == false) {
+      setCursor(cursorRes.latestId - (pageIndex - 1) * 10);
+      setMaxPage(Math.ceil(cursorRes.latestId / 10));
+      setShouldFetchImg(true);
+    }
+  }, [cursorRes, cursorValidating]);
+
+  useEffect(() => {
+    if (category) {
+      setCategoryParams(category === "All" ? "" : `&category=${category}`);
+    }
+  }, [category]);
+
+  useEffect(() => {
+    if (imgRes) {
+      const formattedImages = imgRes.images.map((img: { link: any; id_on_chain: string; category: string }) => ({
         image: img.link,
         link: "https://bodhi.wtf/" + img.id_on_chain,
         category: img.category || "",
         id: img.id_on_chain || 0,
       }));
       setImages(formattedImages);
-    } catch (error) {
-      console.error("Failed to fetch images:", error);
     }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchImages(pageIndex);
-  }, [pageIndex, category]);
-
+  }, [imgRes]);
   return (
     <>
       <div className="flex items-center h-[5rem] px-[2rem] justify-end">
